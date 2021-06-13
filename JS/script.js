b9d26cebd7f0d6e8f4f33224b15e3a28cc80ea47
Katya_Fedorovka <kats@list.ru>
2021-06-13 13:41:12 +0300

    Lesson_13_condition

--- JS/script.js
+++ JS/script.js
@@ -1,38 +1,43 @@
 'use strict';
 
-let num = 50;
-
-// while(num <= 55){
-//     console.log(num);
-//     num++;
-// }
-
-//  do {
-//     console.log(num);
-//     num++; 
-//  }
-//  while (num <55);
-
-//  for (let i = 1; i < 8; i++){
-//      console.log(i);
-//  }
-
-//  for (let i = 1; i < 8; i++){
-//     console.log(num);
-//     num++; 
-// }
-
-for (let i = 1; i < 10; i++){
-    if (i === 6) {
-        // break;
-        continue;
-    }
-
-    console.log(i);
+if (4 == 9) {
+     console.log('Ok!');
+} else {
+    console.log('Error!');
 }
-//  let i = 1 задаем переменную i (сокращенно от итератор)
-//  i < 8  условие, при котором цикл остановит свою работу
-//  в данном случае будет 7 повторений (итераций)
-//  i++  шаг цикла
 
+const num = 50 ;
 
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100){
+    console.log('Too much');
+} else {
+    console.log('Ok!');
+}
+
+// Тернарный оператор (три аргумента):
+// имеется условие:
+// num равно 50
+// ? - это тернарный оператор
+// console.log('Ok!') - действие, которое происходит в случае правдивого условия
+// console.log('Error') - действие, которое выполниться если это условие не выполнилось
+
+(num === 50) ? console.log('Ok!') : console.log('Error');
+
+const num = 50;
+// switch всегда на строгое сравнение
+switch (num) {
+    case 49:
+        console.log('Неверно');
+        break;
+    case 100:
+        console.log('Неверно');
+        break;
+    case 50:
+        console.log('В точку!');
+        break; 
+    default:
+        console.log('Не в этот раз!');
+        break;
+}
