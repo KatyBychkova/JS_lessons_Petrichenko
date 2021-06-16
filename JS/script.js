492abc1a677f57e42bc4f63ecc880c6bd3aa5d8d
Katya_Fedorovka <kats@list.ru>
2021-06-16 12:40:47 +0300

    lesson_16_fuctions

--- JS/script.js
+++ JS/script.js
@@ -1,43 +1,48 @@
 'use strict';
 
-if (4 == 9) {
-     console.log('Ok!');
-} else {
-    console.log('Error!');
-}
+let num = 20;
 
-const num = 50 ;
+  function showFirstMessage(text) {
+      console.log(text);
+      let num = 10;
+      console.log(num);
+  }
+  showFirstMessage("Hello world!");  
+  console.log(num);
 
-if (num < 49) {
-    console.log('Error');
-} else if (num > 100){
-    console.log('Too much');
-} else {
-    console.log('Ok!');
-}
 
-// Тернарный оператор (три аргумента):
-// имеется условие:
-// num равно 50
-// ? - это тернарный оператор
-// console.log('Ok!') - действие, которое происходит в случае правдивого условия
-// console.log('Error') - действие, которое выполниться если это условие не выполнилось
+  function calc(a, b) {
+      return (a + b);
+  }
+  console.log(calc(4, 3));
+  console.log(calc(5, 7));
+  console.log(calc(104, 6));
 
-(num === 50) ? console.log('Ok!') : console.log('Error');
 
-const num = 50;
-// switch всегда на строгое сравнение
-switch (num) {
-    case 49:
-        console.log('Неверно');
-        break;
-    case 100:
-        console.log('Неверно');
-        break;
-    case 50:
-        console.log('В точку!');
-        break; 
-    default:
-        console.log('Не в этот раз!');
-        break;
-}
+  function ret() {
+      let num = 50;
+
+      //
+
+
+      return num; /*возвращает num во внешний мир*/
+  }
+  const anotherNum = ret();
+  console.log(anotherNum);
+
+
+  const logger = function() {
+      console.log("Hello");
+  };
+  logger();
+
+
+  const calc = (a, b) => { return a + b };
+  const calc = (a, b) =>  a + b ;
+
+  const calc = (a, b) => { 
+     console.log('1');
+    return a + b;
+  };
+
+
